b00db15cf802bdf024ad213694303acd2a78e498
Pablo Cabrera <Angelcruz9@Pablos-MacBook-Pro.local>
2022-09-05 13:30:02 -0400

    third week project

---
+++ folders/Week-03-Arrays_and_Functions/project1/index.js
@@ -0,0 +1,16 @@
+
+let itemPrice = 600;
+let amountOfMoney = 1000;
+let numOfFriends =7 ;
+let age = 24 ;
+let firstName ='Pablo';
+let middleName ='Angel';
+let lastName ='Cabrera';
+console.log("The price for the new tv is " + itemPrice);
+console.log('You currently have ' + amountOfMoney );
+console.log("But after buying the tv you now have ");
+console.log(amountOfMoney - itemPrice);
+let fullName = firstName +" " + middleName + " " + lastName;
+console.log("Hey my name is " + fullName);
+console.log('I am currently ' + age + " year's old");
+console.log('I have a small circle of friends the number of people i consider close friends are ' + numOfFriends);
